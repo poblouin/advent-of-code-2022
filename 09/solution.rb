e9cadd47336768e08d877abf36172b913a0c6726
poblouin <poblouin@protonmail.com>
2022-12-09 22:26:29 -0500

    Day 09: Part 2

--- 09/solution.rb
+++ 09/solution.rb
@@ -1,42 +1,76 @@
 require '../input_reader'
 
-NEIGHBORS = [[0, 0], [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]].freeze
-
 def format_input
   InputReader.read.map(&:split)
 end
 
 def adjacent?(head, tail)
-  NEIGHBORS.map { |x, y| [head[0] + x, head[1] + y] }.include?(tail)
+  ((tail[0] - head[0]).abs <= 1 && (tail[1] - head[1]).abs <= 1) || (head[0] == tail[0] && head[1] == tail[1])
 end
 
-def move_tail(head, tail)
-  NEIGHBORS.map { |x, y| [tail[0] + x, tail[1] + y] }.find { |new_tail| adjacent?(head, new_tail) && (new_tail[0] == head[0] || new_tail[1] == head[1]) }
+def move(head, tail)
+  return if adjacent?(head, tail)
+
+  delta_x = head[0] - tail[0]
+  delta_y = head[1] - tail[1]
+  delta_x /= 2 if delta_x.abs == 2
+  delta_y /= 2 if delta_y.abs == 2
+
+  tail[0] += delta_x
+  tail[1] += delta_y
 end
 
 def part1(formatted_input)
-  head = [10000, 10000]
-  tail = [10000, 10000]
-  visited = [[10000, 10000]]
+  knots = Array.new(2) { [1000, 1000] }
+  visited = []
 
   formatted_input.each do |direction, step_count|
     step_count.to_i.times do
       case direction
       when 'U'
-        head[1] += 1
+        knots.first[1] += 1
       when 'L'
-        head[0] -= 1
+        knots.first[0] -= 1
       when 'D'
-        head[1] -= 1
+        knots.first[1] -= 1
       when 'R'
-        head[0] += 1
+        knots.first[0] += 1
       end
 
-      tail = visited.push(adjacent?(head, tail) ? tail : move_tail(head, tail)).last
+      move(knots.first, knots.last)
+      visited.push(knots.last.join)
     end
   end
 
-  visited.map(&:join).uniq.size
+  visited.uniq.size
+end
+
+def part2(formatted_input)
+  knots = Array.new(10) { [1000, 1000] }
+  visited = []
+
+  formatted_input.each do |direction, step_count|
+    step_count.to_i.times do
+      case direction
+      when 'U'
+        knots.first[1] += 1
+      when 'L'
+        knots.first[0] -= 1
+      when 'D'
+        knots.first[1] -= 1
+      when 'R'
+        knots.first[0] += 1
+      end
+
+      knots.each_cons(2) do |previous, current|
+        move(previous, current)
+      end
+      visited.push(knots.last.join)
+    end
+  end
+
+  visited.uniq.size
 end
 
 puts part1(format_input)
+puts part2(format_input)
